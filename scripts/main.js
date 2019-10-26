cf4aee3955a82e9f34da0a3eb04cf37c2a829638
unknown <sandyakccc@gmail.com>
2019-10-26 22:51:58 +0545

    removed butons from main.js file

--- scripts/main.js
+++ scripts/main.js
@@ -8,19 +8,3 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','test-icon.jpg');
     }
 }
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-function setUserName() {
-    let myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Daisies are beautiful, ' + myName;
-  }
-  if(!localStorage.getItem('name')) {
-    setUserName();
-  } else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Daisies are beautiful, ' + storedName;
-  }
-  myButton.onclick = function() {
-    setUserName();
-  }
